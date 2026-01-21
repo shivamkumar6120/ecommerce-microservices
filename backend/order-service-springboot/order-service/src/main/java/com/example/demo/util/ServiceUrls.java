681d41cd0b0585d34d66f664a86bead6babef0eb
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-22 02:12:48 +0530

    serviceUrls added

---
+++ backend/order-service-springboot/order-service/src/main/java/com/example/demo/util/ServiceUrls.java
@@ -0,0 +1,8 @@
+package com.example.demo.util;
+
+public class ServiceUrls {
+
+    public static final String CART_SERVICE = "http://localhost:8083/cart";
+    public static final String PRODUCT_SERVICE = "http://localhost:8082/products";
+    public static final String PAYMENT_SERVICE = "http://localhost:8085/payment";
+}
