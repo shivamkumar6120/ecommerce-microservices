6abdff7fd386a033c80c51860c1c380e1795924f
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-21 13:09:25 +0530

    dtos added

---
+++ backend/auth-service-springboot/auth-service/src/main/java/com/example/auth/dto/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.example.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+}
