d1d790dbbe8821e32062204ad1d484089f700fa6
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-22 01:29:23 +0530

    index.js completed

--- backend/product-service-express/index.js
+++ backend/product-service-express/index.js
@@ -1,12 +1,23 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const productRoutes = require("./routes/productRoutes");
+
 const app = express();
 
+
+
+app.use("/products", productRoutes);
+
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => {
   res.send("Product Service Running");
 });
 
-app.listen(8082, () => {
-  console.log("Product Service running on 8082");
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => {
+  console.log(`Product Service running on port ${PORT}`);
 });
