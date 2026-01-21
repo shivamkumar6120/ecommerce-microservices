9bb930957f9639728202a5c40775134d93c1d830
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-22 02:43:21 +0530

    jwt implemented

--- backend/payment-service-dotnet/Controllers/PaymentController.cs
+++ backend/payment-service-dotnet/Controllers/PaymentController.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using PaymentService.Models;
 
 namespace PaymentService.Controllers
 {
     [ApiController]
     [Route("payment")]
+    [Authorize] // JWT REQUIRED
     public class PaymentController : ControllerBase
     {
         [HttpPost("pay")]
         public IActionResult Pay([FromBody] PaymentRequest request)
         {
-            // Simulate payment logic
             if (request.Amount <= 0)
             {
                 return BadRequest(new { status = "FAILED" });
