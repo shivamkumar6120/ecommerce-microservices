9e65538c2b79fd60b7abb9fbae6882f3e5b77e3f
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-22 02:01:38 +0530

    index of cart  servicce

--- backend/cart-service-express/index.js
+++ backend/cart-service-express/index.js
@@ -1,12 +1,20 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const cors = require("cors");
 
+const cartRoutes = require("./routes/cartRoutes");
+
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get("/cart-service", (req, res) => {
-  res.send("Product Service Running");
+app.get("/health", (req, res) => {
+  res.send("Cart Service Running");
 });
 
-app.listen(8083, () => {
-  console.log("Product Service running on 8083");
+app.use("/cart", cartRoutes);
+
+const PORT = process.env.PORT || 8083;
+app.listen(PORT, () => {
+  console.log(`Cart Service running on port ${PORT}`);
 });
