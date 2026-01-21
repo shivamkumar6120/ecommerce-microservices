bccef24ad4050353d09696b0a2954dc3a9a4ad42
shivamkumar6120 <nirajshivam6120@gmail.com>
2026-01-21 13:26:19 +0530

    corrected some lines

--- backend/auth-service-springboot/auth-service/src/main/java/com/example/auth/config/SecurityConfig.java
+++ backend/auth-service-springboot/auth-service/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -7,21 +7,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.auth.security.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
+            	.requestMatchers("/auth/login", "/auth/register").permitAll()
+                .anyRequest().authenticated()
             )
             .httpBasic(Customizer.withDefaults())
-            .formLogin(form -> form.disable());
-
+            .formLogin(form -> form.disable())
+            .addFilterBefore(jwtAuthenticationFilter, 
+            		  UsernamePasswordAuthenticationFilter.class);
+        
         return http.build();
     }
 
